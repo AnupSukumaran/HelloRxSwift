ca5f20fcd2efde260312f192c93641aa8b014b54
Anup Sukumaran <anup.sukumaran9@gmail.com>
2020-08-25 22:19:10 +0530

    - relay variables

--- HelloRxSwift/RXSwiftPlayground.playground/Contents.swift
+++ HelloRxSwift/RXSwiftPlayground.playground/Contents.swift
@@ -3,21 +3,37 @@ import RxSwift
 import RxRelay
 let disposeBag = DisposeBag()
 
-let variable =  Variable("inital value")
+let relay = BehaviorRelay(value: "Inital Value")
 
-let variableArr =  Variable([String]())
 
-variable.value = "Hello world"
+relay.asObservable()
+    .subscribe{print("val = \($0)")}
 
-variable.asObservable()
-    .subscribe {
-        print("val = \($0)")
-}
+relay.accept("New1")
+relay.accept("New2")
 
-variableArr.asObservable()
-    .subscribe {
-        print("val = \($0)")
-}
+let relayArr = BehaviorRelay(value: [String]())
 
-variableArr.value.append("Item1")
-variableArr.value.append("Item2")
+
+relayArr.accept(["NewVal1"])
+relayArr.accept(["NewVal1", "NewVal2", "NewVal3"])
+
+relayArr.asObservable()
+.subscribe{print("val1 = \($0)")}
+
+
+let relayArr2 = BehaviorRelay(value: ["NewTest"])
+
+
+relayArr2.accept(relayArr2.value + ["NewVal1"])
+
+
+relayArr2.asObservable()
+.subscribe{print("val2 = \($0)")}
+
+var transVal = relayArr2.value
+
+transVal.append("NewForm1")
+transVal.append("NewForm2")
+
+relayArr2.accept(transVal)
